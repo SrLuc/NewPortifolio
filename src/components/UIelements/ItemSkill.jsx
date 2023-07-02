3e2417d36c0f8a87108a81fed5b23ff263b1c0a4
unknown <ruanlucenaramos@gmail.com>
2023-07-02 12:11:24 -0300

    style:add style for text of ItemSKills | chore: add React Icons library

--- src/components/UIelements/ItemSkill.jsx
+++ src/components/UIelements/ItemSkill.jsx
@@ -1,8 +1,10 @@
 const ItemSkill = ({ icon, title, children }) => {
   return (
-    <article className="xl:w-56 lg:w-56 md:w-56 flex flex-col justify-evenly border-2 m-1 p-1 border-[#191E29] min-[440px]:flex min-[440px]:w-full ">
-      <i>{icon}</i>
-      <h3 className="font-bold text-[#191E29]">{title}</h3>
+    <article className="transition xl:w-56 lg:w-56 md:w-56 flex flex-col justify-evenly border-2 m-1 p-1  border-[#191E29] hover:bg-gray-300 min-[440px]:flex min-[440px]:w-full ">
+      <div className="flex gap-2 align-middle">
+        <i>{icon}</i>
+        <h3 className="font-bold text-[#191E29]">{title}</h3>
+      </div>
       <p className="text-sm font-bold text-left">{children}</p>
     </article>
   );
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,36 +1,45 @@
 import React from "react";
+import {GrReactjs} from "react-icons/gr";
+import {SiNodedotjs} from "react-icons/si";
+import {TbBrandMysql}  from "react-icons/tb";
+import {BiLogoTypescript} from "react-icons/bi";
+import {SiTailwindcss} from "react-icons/si";
+import {RiEnglishInput} from "react-icons/ri";
 import ItemSkill from "../components/UIelements/ItemSkill";
+
+
+
 const Skills = () => {
   return (
     <>
       <section className="flex justify-around p-1 w-full h-full align-middle flex-wrap min-[440px]:p-2">
-        <ItemSkill icon="test" title="ReactJS">
+        <ItemSkill icon={<GrReactjs size={25}/>} title="ReactJS">
           I have solid ReactJS skills, with experience building reusable
           components, managing state, and integrating with external APIs. I also
           have knowledge in advanced concepts like routing with React Router and
           server-side rendering with Next.js
         </ItemSkill>
-        <ItemSkill icon="test" title="NodeJS">
-          I have knowledge in Node.js. In addition, I have knowledge in NoSQL
+        <ItemSkill icon={<SiNodedotjs size={25}/>}title="NodeJS">
+          I have knowledge in Node.js and in addition, I have knowledge in NoSQL
           databases, such as MongoDB, and in tools such as Express.js for
-          creating routes and controlling middlewares.
+          creating routes, integration with API'S RestFULL using Axios Module for control middlewares.
         </ItemSkill>
-        <ItemSkill icon="test" title="MySQL">
+        <ItemSkill icon={<TbBrandMysql size={25}/>} title="MySQL">
           I have experience in data modeling using tools such as Power BI,
           brModelo and MySQL Workbench. I am able to design efficient and
           structured database schemas and I also have knowledge in advanced queries in SQL.
         </ItemSkill>
-        <ItemSkill icon="test" title="Typescript">
-          I have knowledge in TypeScript and Styled Components. I have
-          experience creating reusable components, managing state with Redux or
+        <ItemSkill icon={<BiLogoTypescript size={25}/>} title="Typescript">
+          I have knowledge in TypeScript and Styled Components Library and I have
+          experience with Design Figma to creating reusable components, managing state with Redux or
           Context API, and navigating with React Router.
         </ItemSkill>
-        <ItemSkill icon="test" title="Tailwind">
+        <ItemSkill icon={<SiTailwindcss size={25}/>} title="Tailwind">
           I have solid skills in using Tailwind CSS In addition, I can take
           advantage of Tailwind's pre-built utilities and components to speed up
           development and ensure visual consistency throughout the project.
         </ItemSkill>
-        <ItemSkill icon="test" title="English">
+        <ItemSkill icon={<RiEnglishInput size={25}/>} title="English">
           I have proficient communication skills in English, although I may find
           it difficult to express myself. However, I have a strong ability to
           understand and comprehend English conversations or written content
