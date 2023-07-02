2b058c945c80c2568087bf0531f52267e63efb9f
unknown <ruanlucenaramos@gmail.com>
2023-07-02 10:54:26 -0300

    style: add selected function to navigationbar

--- src/components/navigation/NavigationBar.jsx
+++ src/components/navigation/NavigationBar.jsx
@@ -1,14 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+const NavItem = ({ to, selected, children }) => {
+  return (
+    <li>
+      <Link
+        to={to}
+        className={` ${selected ? "text-[#C0FF00]" : "text-gray-50"}`}
+      >
+        {children}
+      </Link>
+    </li>
+  );
+};
 
 const NavigationBar = () => {
+  const location = useLocation();
+
   return (
-    <nav className="bg-[#191E29] font-bold text-gray-100 h-full p-5" >
-      <ul className="flex justify-around ">
-        <Link to="/">Skills</Link>
-        <Link to="/about">About</Link>
-        <Link to="/works">Works</Link>
-        <Link to="/contact">Contact</Link>
+    <nav className="bg-[#191E29] font-bold text-gray-100 h-full p-5">
+      <ul className="flex justify-around shadow-lg">
+        <NavItem to="/" selected={location.pathname === "/"}>
+          Skills
+        </NavItem>
+        <NavItem to="/about" selected={location.pathname === "/about"}>
+          About
+        </NavItem>
+        <NavItem to="/works" selected={location.pathname === "/works"}>
+          Works
+        </NavItem>
+        <NavItem to="/contact" selected={location.pathname === "/contact"}>
+          Contact
+        </NavItem>
       </ul>
     </nav>
   );
